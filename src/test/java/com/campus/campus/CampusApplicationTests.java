c51863c57c8d7e485b69a5fd64e498a6093aa6e3
1winhyun <jimking1@naver.com>
2025-11-12 21:37:24 +0900

    feat: ci.yaml 작성 및 헬스체크 테스트 구현

--- src/test/java/com/campus/campus/CampusApplicationTests.java
+++ src/test/java/com/campus/campus/CampusApplicationTests.java
@@ -1,10 +1,30 @@
 package com.campus.campus;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.assertj.core.api.Assertions.*;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class CampusApplicationTests {
+	@LocalServerPort
+	int port;
+
+	@Autowired
+	TestRestTemplate restTemplate;
+
+	@Test
+	void healthCheck() {
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/actuator/health",
+			String.class);
+		assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
+	}
 
 	@Test
 	void contextLoads() {
