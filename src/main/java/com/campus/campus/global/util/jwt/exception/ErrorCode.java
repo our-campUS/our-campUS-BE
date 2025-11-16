e6391511276cd99b7bee39dfb6725d7b89b8c20a
1winhyun <jimking1@naver.com>
2025-11-16 22:02:45 +0900

    feat: JwtAuthenticator 구현

---
+++ src/main/java/com/campus/campus/global/util/jwt/exception/ErrorCode.java
@@ -0,0 +1,19 @@
+package com.campus.campus.global.util.jwt.exception;
+
+import org.springframework.http.HttpStatus;
+
+import com.campus.campus.global.common.exception.ErrorCodeInterface;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum ErrorCode implements ErrorCodeInterface {
+	JWT_INVALID(2001, HttpStatus.NOT_FOUND, "유효하지 않은 JWT 토큰입니다."),
+	JWT_EXPIRED(2002, HttpStatus.BAD_REQUEST, "만료된 JWT 토큰입니다.");
+
+	private final int code;
+	private final HttpStatus status;
+	private final String message;
+}
