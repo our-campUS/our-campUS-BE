f1101cd549c969826d33084159b87f27428ef02f
1winhyun <jimking1@naver.com>
2025-11-16 22:02:07 +0900

    feat: User 엔티티 구현

---
+++ src/main/java/com/campus/campus/domain/user/domain/entity/User.java
@@ -0,0 +1,47 @@
+package com.campus.campus.domain.user.domain.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "user_id")
+	private Long id;
+
+	@Column(name = "kakao_id")
+	private Long kakaoId;
+
+	@Column(name = "name")
+	private String nickname;
+
+	@Column(name = "email")
+	private String email;
+
+	@Column(name = "profile_image")
+	private String profileImage;
+
+	public static User createUser(Long kakaoId, String nickname, String email, String profileImage) {
+		return User.builder()
+			.kakaoId(kakaoId)
+			.nickname(nickname)
+			.email(email)
+			.profileImage(profileImage)
+			.build();
+	}
+}
