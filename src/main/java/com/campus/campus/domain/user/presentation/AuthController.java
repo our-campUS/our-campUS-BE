b41ec70f1c1907987a7f4f20c83800c2a0595ce0
1winhyun <jimking1@naver.com>
2025-11-16 22:05:53 +0900

    feat: 로그인 controller 구현

---
+++ src/main/java/com/campus/campus/domain/user/presentation/AuthController.java
@@ -0,0 +1,26 @@
+package com.campus.campus.domain.user.presentation;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.campus.campus.global.auth.application.dto.OauthLoginResponse;
+import com.campus.campus.domain.user.application.service.KakaoOauthService;
+import com.campus.campus.global.common.response.CommonResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/auth/login")
+public class AuthController {
+	private final KakaoOauthService kakaoOauthService;
+
+	@PostMapping("/kakao")
+	public CommonResponse<OauthLoginResponse> kakaoLogin(@RequestParam("code") String code) {
+		OauthLoginResponse response = kakaoOauthService.login(code);
+
+		return CommonResponse.success(UserResponseCode.LOGIN_SUCCESS, response);
+	}
+}
