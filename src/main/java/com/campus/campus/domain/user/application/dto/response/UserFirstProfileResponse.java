10337a23376dac3eeccf1ea49661331c5f6e82fa
1winhyun <jimking1@naver.com>
2025-11-19 00:53:35 +0900

    refactor: UserFirstProfileResponse 스키마 추가

--- src/main/java/com/campus/campus/domain/user/application/dto/response/UserFirstProfileResponse.java
+++ src/main/java/com/campus/campus/domain/user/application/dto/response/UserFirstProfileResponse.java
@@ -1,11 +1,17 @@
 package com.campus.campus.domain.user.application.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 
 @Builder
 public record UserFirstProfileResponse(
+	@Schema(description = "학교 이름", example = "가천대학교")
 	String schoolName,
+
+	@Schema(description = "단과대 이름", example = "IT융합대학")
 	String collegeName,
+
+	@Schema(description = "학과 이름", example = "컴퓨터공학과")
 	String majorName
 ) {
 }
