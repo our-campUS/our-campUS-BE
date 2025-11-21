8b183bfaf251f931b2639ca9e4d68dd5c1164902
1winhyun <jimking1@naver.com>
2025-11-21 22:15:22 +0900

    refactor: OauthLoginResponse dto 스키마 추가

--- src/main/java/com/campus/campus/global/auth/application/dto/OauthLoginResponse.java
+++ src/main/java/com/campus/campus/global/auth/application/dto/OauthLoginResponse.java
@@ -1,13 +1,30 @@
 package com.campus.campus.global.auth.application.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record OauthLoginResponse(
+	@Schema(description = "인증된 access token", example = "랜덤 accessToken")
 	String accessToken,
+
+	@Schema(description = "인증된 refresh token", example = "랜덤 refreshToken")
 	String refreshToken,
+
+	@Schema(description = "사용자 이름(닉네임)", example = "김잇타")
 	String nickname,
+
+	@Schema(description = "사용자 id", example = "1")
 	Long userId,
+
+	@Schema(description = "사용자 kakaoId", example = "4548160187")
 	Long kakaoId,
+
+	@Schema(description = "사용자 이메일", example = "itta@naver.com")
 	String email,
+
+	@Schema(description = "사용자 프로필 이미지", example = "http://image/img_640x640.jpg")
 	String profileImage,
+
+	@Schema(description = "사용자 프로필(학적 정보) 작성 여부", example = "false")
 	boolean isProfileNotCompleted
 ) {
 }
