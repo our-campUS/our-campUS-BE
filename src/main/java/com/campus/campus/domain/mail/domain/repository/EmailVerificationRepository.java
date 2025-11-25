1e821d8d91ec34b8785fe2c1557ccd983ba01187
1winhyun <jimking1@naver.com>
2025-11-25 21:57:01 +0900

    feat: 이메일 인증 관련 repository 구현

---
+++ src/main/java/com/campus/campus/domain/mail/domain/repository/EmailVerificationRepository.java
@@ -0,0 +1,11 @@
+package com.campus.campus.domain.mail.domain.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.campus.campus.domain.mail.domain.entity.EmailVerification;
+
+public interface EmailVerificationRepository extends JpaRepository<EmailVerification, Long> {
+	Optional<EmailVerification> findTopByEmailOrderByEmailVerificationIdDesc(String email);
+}
