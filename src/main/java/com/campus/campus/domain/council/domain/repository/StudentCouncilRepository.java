c9803fa144067ce394db76baedfad5ab6593f835
1winhyun <jimking1@naver.com>
2025-11-25 18:56:37 +0900

    feat: StudentCouncilRepository 구현

---
+++ src/main/java/com/campus/campus/domain/council/domain/repository/StudentCouncilRepository.java
@@ -0,0 +1,13 @@
+package com.campus.campus.domain.council.domain.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.campus.campus.domain.council.domain.entity.StudentCouncil;
+
+public interface StudentCouncilRepository extends JpaRepository<StudentCouncil, Long> {
+	Optional<StudentCouncil> findByLoginId(String loginId);
+
+	boolean existsByLoginId(String loginId);
+}
