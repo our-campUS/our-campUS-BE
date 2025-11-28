67ed868f4776a826016344de019dbeb06042d00d
1winhyun <jimking1@naver.com>
2025-11-28 23:53:01 +0900

    feat: 학교 검색 service 구현

---
+++ src/main/java/com/campus/campus/domain/school/application/service/SchoolService.java
@@ -0,0 +1,28 @@
+package com.campus.campus.domain.school.application.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.campus.campus.domain.school.application.dto.request.SchoolFindRequest;
+import com.campus.campus.domain.school.application.dto.response.SchoolFindResponse;
+import com.campus.campus.domain.school.application.mapper.SchoolFindMapper;
+import com.campus.campus.domain.school.domain.entity.School;
+import com.campus.campus.domain.school.domain.repository.SchoolRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class SchoolService {
+	private final SchoolRepository schoolRepository;
+	private final SchoolFindMapper schoolFindMapper;
+
+	public List<SchoolFindResponse> searchSchools(SchoolFindRequest schoolFindRequest) {
+		List<School> schools = schoolRepository.findBySchoolNameStartingWith(schoolFindRequest.searchWord());
+
+		return schoolFindMapper.toSchoolFindResponseList(schools);
+	}
+}
