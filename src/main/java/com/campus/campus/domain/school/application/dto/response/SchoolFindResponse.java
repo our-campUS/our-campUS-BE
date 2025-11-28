22949183a2d3ce342ad9f727e899393538026a9f
1winhyun <jimking1@naver.com>
2025-11-28 23:43:11 +0900

    feat: 학교 검색 dto 구현

---
+++ src/main/java/com/campus/campus/domain/school/application/dto/response/SchoolFindResponse.java
@@ -0,0 +1,12 @@
+package com.campus.campus.domain.school.application.dto.response;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+
+public record SchoolFindResponse(
+	@Schema(description = "학교 id", example = "3")
+	String schoolId,
+
+	@Schema(description = "학교 이름", example = "가천대학교")
+	String schoolName
+) {
+}
