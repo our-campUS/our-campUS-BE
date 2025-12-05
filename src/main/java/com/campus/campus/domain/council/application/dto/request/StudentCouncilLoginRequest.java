04ddd82b6d9e91d072e0fd6ed2b368490490cb97
1winhyun <jimking1@naver.com>
2025-12-05 23:24:40 +0900

    refactor: 학생회 회원가입 비밀번호 최소 글자 Size 어노테이션 추가

--- src/main/java/com/campus/campus/domain/council/application/dto/request/StudentCouncilLoginRequest.java
+++ src/main/java/com/campus/campus/domain/council/application/dto/request/StudentCouncilLoginRequest.java
@@ -2,6 +2,7 @@ package com.campus.campus.domain.council.application.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 public record StudentCouncilLoginRequest(
 	@Schema(description = "회원가입할 이메일(id)", example = "campus@campus.com")
@@ -10,6 +11,7 @@ public record StudentCouncilLoginRequest(
 
 	@Schema(description = "비밀번호", example = "qwerqwer")
 	@NotBlank
+	@Size(min = 8, message = "비밀번호는 최소 8자 이상이어야 합니다.")
 	String password
 ) {
 }
