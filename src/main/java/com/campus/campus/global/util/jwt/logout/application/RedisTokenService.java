027d1a9f7c62b619e1032ecf07bdafe815fe32d7
1winhyun <jimking1@naver.com>
2025-12-18 13:54:20 +0900

    feat: Redis에 토큰 저장 및 블랙리스트 추가 로직 구현

---
+++ src/main/java/com/campus/campus/global/util/jwt/logout/application/RedisTokenService.java
@@ -0,0 +1,43 @@
+package com.campus.campus.global.util.jwt.logout.application;
+
+import java.util.concurrent.TimeUnit;
+
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class RedisTokenService {
+
+	private final RedisTemplate<String, Object> redisTemplate;
+
+	// Refresh Token 저장
+	public void setRefreshToken(String role, String id, String refreshToken, long expirationSeconds) {
+		String key = "RT:" + role + ":" + id;
+		redisTemplate.opsForValue().set(key, refreshToken, expirationSeconds, TimeUnit.SECONDS);
+	}
+
+	// Refresh Token 가져오기
+	public String getRefreshToken(String role, String id) {
+		String key = "RT:" + role + ":" + id;
+		return (String) redisTemplate.opsForValue().get(key);
+	}
+
+	// Refresh Token 삭제
+	public void deleteRefreshToken(String role, String id) {
+		String key = "RT:" + role + ":" + id;
+		redisTemplate.delete(key);
+	}
+
+	// BlackList 추가 (Key: accessToken, Value: "logout")
+	public void setBlackList(String accessToken, String msg, Long milliSeconds) {
+		redisTemplate.opsForValue().set(accessToken, msg, milliSeconds, TimeUnit.MILLISECONDS);
+	}
+
+	// BlackList 확인
+	public boolean hasKeyBlackList(String key) {
+		return Boolean.TRUE.equals(redisTemplate.hasKey(key));
+	}
+}
