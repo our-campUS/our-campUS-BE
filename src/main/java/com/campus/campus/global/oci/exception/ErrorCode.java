b1d91441d4051ac1a9b7d181fca6fbaee402a436
starday119 <jy8294@naver.com>
2026-01-03 15:20:12 +0900

    fix/#23: Oci ErrorCode 추가

--- src/main/java/com/campus/campus/global/oci/exception/ErrorCode.java
+++ src/main/java/com/campus/campus/global/oci/exception/ErrorCode.java
@@ -11,7 +11,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum ErrorCode implements ErrorCodeInterface {
 
-	OCI_PRESIGNED_URL_CREATE_FAILED(6100, HttpStatus.INTERNAL_SERVER_ERROR, "Presigned URL 생성에 실패했습니다.");
+	OCI_PRESIGNED_URL_CREATE_FAILED(6100, HttpStatus.INTERNAL_SERVER_ERROR, "Presigned URL 생성에 실패했습니다."),
+	OCI_OBJECT_DELETE_FAILED(6101, HttpStatus.INTERNAL_SERVER_ERROR, "이미지 삭제 중 오류가 발생했습니다.");
 
 	private final int code;
 	private final HttpStatus status;
