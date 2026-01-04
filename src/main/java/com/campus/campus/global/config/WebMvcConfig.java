687c944bcc31321501055355ee1cc1d0e9813452
1winhyun <jimking1@naver.com>
2026-01-05 01:34:04 +0900

    refactor: WebMvcConfig에 CurrentManagerIdArgumentResolver 추가

--- src/main/java/com/campus/campus/global/config/WebMvcConfig.java
+++ src/main/java/com/campus/campus/global/config/WebMvcConfig.java
@@ -7,6 +7,7 @@ import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.campus.campus.global.annotation.CurrentCouncilIdArgumentResolver;
+import com.campus.campus.global.annotation.CurrentManagerIdArgumentResolver;
 import com.campus.campus.global.annotation.CurrentUserIdArgumentResolver;
 
 import lombok.RequiredArgsConstructor;
@@ -17,10 +18,12 @@ public class WebMvcConfig implements WebMvcConfigurer {
 
 	private final CurrentUserIdArgumentResolver currentUserIdArgumentResolver;
 	private final CurrentCouncilIdArgumentResolver currentCouncilIdArgumentResolver;
+	private final CurrentManagerIdArgumentResolver currentManagerIdArgumentResolver;
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(currentUserIdArgumentResolver);
 		resolvers.add(currentCouncilIdArgumentResolver);
+		resolvers.add(currentManagerIdArgumentResolver);
 	}
 }
