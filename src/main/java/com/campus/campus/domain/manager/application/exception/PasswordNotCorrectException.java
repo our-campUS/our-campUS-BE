811b7005f4597488b0a3718bc9eb65d77714cb16
1winhyun <jimking1@naver.com>
2026-01-05 14:11:25 +0900

    refactor: manager의 PasswordNotCorrectException import 경로 수정

--- src/main/java/com/campus/campus/domain/manager/application/exception/PasswordNotCorrectException.java
+++ src/main/java/com/campus/campus/domain/manager/application/exception/PasswordNotCorrectException.java
@@ -1,6 +1,5 @@
 package com.campus.campus.domain.manager.application.exception;
 
-import com.campus.campus.domain.council.application.exception.ErrorCode;
 import com.campus.campus.global.common.exception.ApplicationException;
 
 public class PasswordNotCorrectException extends ApplicationException {
