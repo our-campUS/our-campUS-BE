cda307d21c89786bc90e99f8b2e0a5cc753190c0
1winhyun <jimking1@naver.com>
2026-01-05 13:59:35 +0900

    refactor: manager 관련 에러코드 변경

--- src/main/java/com/campus/campus/domain/manager/application/exception/ErrorCode.java
+++ src/main/java/com/campus/campus/domain/manager/application/exception/ErrorCode.java
@@ -10,8 +10,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ErrorCode implements ErrorCodeInterface {
-	MANAGER_NOT_FOUND(2601, HttpStatus.NOT_FOUND, "해당 관리자는 존재하지 않습니다."),
-	PASSWORD_NOT_CORRECT(2602, HttpStatus.UNAUTHORIZED, "비밀번호가 틀렸습니다.");
+	MANAGER_NOT_FOUND(2701, HttpStatus.NOT_FOUND, "해당 관리자는 존재하지 않습니다."),
+	PASSWORD_NOT_CORRECT(2702, HttpStatus.UNAUTHORIZED, "비밀번호가 틀렸습니다.");
 
 	private final int code;
 	private final HttpStatus status;
