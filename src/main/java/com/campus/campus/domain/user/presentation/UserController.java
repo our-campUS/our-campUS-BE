fa7d37fad85c5cedb43f91bdf3c6484f461b9c03
1winhyun <jimking1@naver.com>
2026-01-08 06:52:00 +0900

    feat: 카카오 이름 외 서비스 내 닉네임 설정이 가능하도록 컬럼 및 로직 추가 (닉네임 변경 구현)

--- src/main/java/com/campus/campus/domain/user/presentation/UserController.java
+++ src/main/java/com/campus/campus/domain/user/presentation/UserController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.campus.campus.domain.user.application.dto.request.CampusNicknameUpdateRequest;
 import com.campus.campus.domain.user.application.dto.request.UserProfileRequest;
 import com.campus.campus.domain.user.application.dto.response.UserFirstProfileResponse;
 import com.campus.campus.domain.user.application.service.UserService;
@@ -28,5 +29,14 @@ public class UserController {
 		UserFirstProfileResponse userFirstProfileResponse = userService.writeUserProfile(userId, userProfileRequest);
 
 		return CommonResponse.success(UserResponseCode.FIRST_PROFILE_WRITE, userFirstProfileResponse);
+	}
+
+	@PatchMapping("/change/nickname")
+	@Operation(summary = "사용자 서비스 내 닉네임 변경")
+	public CommonResponse<Void> updateCampusNickname(@CurrentUserId Long userId,
+		@Valid @RequestBody CampusNicknameUpdateRequest nicknameUpdateRequest) {
+		userService.updateCampusNickname(userId, nicknameUpdateRequest);
+
+		return CommonResponse.success(UserResponseCode.NICKNAME_UPDATE_SUCCESS);
 	}
 }
--- src/main/java/com/campus/campus/domain/user/presentation/UserResponseCode.java
+++ src/main/java/com/campus/campus/domain/user/presentation/UserResponseCode.java
@@ -12,6 +12,7 @@ import lombok.Getter;
 public enum UserResponseCode implements ResponseCodeInterface {
 	LOGIN_SUCCESS(200, HttpStatus.OK, "로그인에 성공했습니다."),
 	FIRST_PROFILE_WRITE(200, HttpStatus.OK, " 프로필(학교 정보) 입력에 성공했습니다."),
+	NICKNAME_UPDATE_SUCCESS(200, HttpStatus.OK, "닉네임 변경에 성공했습니다."),
 	WITHDRAW_SUCCESS(200, HttpStatus.OK, "회원탈퇴에 성공했습니다.");
 
 	private final int code;
