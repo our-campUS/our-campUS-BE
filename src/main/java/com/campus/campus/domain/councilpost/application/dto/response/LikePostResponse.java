682f91033e92e4263b66cda8351636760c8834a4
1winhyun <jimking1@naver.com>
2026-01-08 20:59:27 +0900

    refactor: LikePostResponse 스키마 수정

--- src/main/java/com/campus/campus/domain/councilpost/application/dto/response/LikePostResponse.java
+++ src/main/java/com/campus/campus/domain/councilpost/application/dto/response/LikePostResponse.java
@@ -3,13 +3,13 @@ package com.campus.campus.domain.councilpost.application.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record LikePostResponse(
-	@Schema(name = "좋아요 누른 사용자 id", example = "1")
+	@Schema(description = "좋아요 누른 사용자 id", example = "1")
 	Long userId,
 
-	@Schema(name = "좋아요 누를 게시글의 id", example = "1")
+	@Schema(description = "좋아요 누를 게시글의 id", example = "1")
 	Long postId,
 
-	@Schema(name = "좋아요 여부", example = "true")
+	@Schema(description = "좋아요 여부", example = "true")
 	boolean liked
 ) {
 }
