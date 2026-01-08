9c0c347d6fe22047a5f1f13d755dec348369de9e
1winhyun <jimking1@naver.com>
2026-01-08 16:25:08 +0900

    feat: 학생회 게시글 좋아요 엔티티 구현

---
+++ src/main/java/com/campus/campus/domain/councilpost/domain/entity/LikePost.java
@@ -0,0 +1,40 @@
+package com.campus.campus.domain.councilpost.domain.entity;
+
+import static jakarta.persistence.FetchType.*;
+
+import com.campus.campus.domain.user.domain.entity.User;
+import com.campus.campus.global.entity.BaseEntity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class LikePost extends BaseEntity {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "like_post_id")
+	private Long likePostId;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@ManyToOne(fetch = LAZY)
+	@JoinColumn(name = "post_id")
+	private StudentCouncilPost post;
+}
