57730c384cfacfb5415a7448204f3bdc6a874d90
1winhyun <jimking1@naver.com>
2026-01-08 20:54:45 +0900

    refactor: LikePost 유니크 제약 조건 추가

--- src/main/java/com/campus/campus/domain/councilpost/domain/entity/LikePost.java
+++ src/main/java/com/campus/campus/domain/councilpost/domain/entity/LikePost.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -21,7 +22,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "like_posts")
+@Table(name = "like_posts", uniqueConstraints =
+	{@UniqueConstraint(columnNames = {"user_id", "post_id"})})
 @Getter
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
