f7c875e16c854f69764220b941bcb66ffbed5c0c
starday119 <jy8294@naver.com>
2026-01-10 07:03:59 +0900

    merge/#45: dev 머지

--- src/main/java/com/campus/campus/global/firebase/application/dto/FcmMessageRequestDto.java
+++ src/main/java/com/campus/campus/global/firebase/application/dto/FcmMessageRequestDto.java
@@ -8,10 +8,10 @@ public record FcmMessageRequestDto(
 	@Schema(description = "유저ID", example = "1")
 	Long userId,
 
-	@Schema(description = "메시지 발송인", example = "중앙대학교 학생회")
+	@Schema(description = "메시지 발송인", example = "시스템")
 	String title,
 
-	@Schema(description = "메시지 내용", example = "새로운 제휴가 등록되었습니다.")
+	@Schema(description = "메시지 내용", example = "이제 서연님에게 화이팅을 할 수 있어요.")
 	String body
 ) {
 }
