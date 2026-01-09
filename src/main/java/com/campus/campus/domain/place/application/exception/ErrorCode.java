728920abc1c44342edbd436dfaaa4b818b113cc7
1winhyun <jimking1@naver.com>
2026-01-10 07:09:32 +0900

    refactor: ErrorCode 코드 중복 수정

--- src/main/java/com/campus/campus/domain/place/application/exception/ErrorCode.java
+++ src/main/java/com/campus/campus/domain/place/application/exception/ErrorCode.java
@@ -16,7 +16,7 @@ public enum ErrorCode implements ErrorCodeInterface {
 	SHA256_NOT_SUPPORTED(2603, HttpStatus.INTERNAL_SERVER_ERROR, "SHA-256이 지원되지 않습니다."),
 	NAVER_API_ERROR(2604, HttpStatus.INTERNAL_SERVER_ERROR, "네이버 api 호출에 실패하였습니다."),
 	PLACE_CREATION_ERROR(2605, HttpStatus.INTERNAL_SERVER_ERROR, "Place 생성에 오류가 발생하였습니다."),
-	GEOCODER_ERROR(2605, HttpStatus.INTERNAL_SERVER_ERROR, "좌표 -> 주소 변환 과정에서 오류가 발생하였습니다..");
+	GEOCODER_ERROR(2606, HttpStatus.INTERNAL_SERVER_ERROR, "좌표 -> 주소 변환 과정에서 오류가 발생하였습니다..");
 
 	private final int code;
 	private final HttpStatus status;
