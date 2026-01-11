ee44b5329b7d358723e11fb889a40eca77c60683
starday119 <jy8294@naver.com>
2026-01-11 16:51:31 +0900

    feat/#50: 필요없는 메서드 제외

--- src/main/java/com/campus/campus/domain/councilpost/application/service/StudentCouncilPostService.java
+++ src/main/java/com/campus/campus/domain/councilpost/application/service/StudentCouncilPostService.java
@@ -31,7 +31,6 @@ import com.campus.campus.domain.councilpost.domain.entity.PostImage;
 import com.campus.campus.domain.councilpost.domain.entity.StudentCouncilPost;
 import com.campus.campus.domain.councilpost.domain.repository.PostImageRepository;
 import com.campus.campus.domain.councilpost.domain.repository.StudentCouncilPostRepository;
-import com.campus.campus.domain.partnership.application.service.PartnershipService;
 import com.campus.campus.domain.place.application.service.PlaceService;
 import com.campus.campus.domain.place.domain.entity.Place;
 import com.campus.campus.global.oci.application.service.PresignedUrlService;
