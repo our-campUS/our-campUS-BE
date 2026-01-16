6d65bada884370f2d0c44ed2f8047cf80181b483
1winhyun <jimking1@naver.com>
2026-01-17 00:44:35 +0900

    refactor: PermitUrlConfig에 url 발급 추가

--- src/main/java/com/campus/campus/global/config/PermitUrlConfig.java
+++ src/main/java/com/campus/campus/global/config/PermitUrlConfig.java
@@ -30,6 +30,7 @@ public class PermitUrlConfig {
 			"/jwt/token/reissue",
 			"/managers/login",
 			"/places/search",
+			"/storage/presigned",
 			"/places",
 			"/api/partnership/list",
 			"/api/partnership/map"
