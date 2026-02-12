65b4d68a6adb582f1635c61de0c73843632d247e
wldy4627 <wldy4627@gmail.com>
2026-02-13 02:31:33 +0900

    feat/#103: 학생회 전용 문의 등록 및 조회 서비스 로직 구현

--- src/main/java/com/campus/campus/domain/inquiry/domain/repository/InquiryRepository.java
+++ src/main/java/com/campus/campus/domain/inquiry/domain/repository/InquiryRepository.java
@@ -5,10 +5,13 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.campus.campus.domain.council.domain.entity.StudentCouncil;
 import com.campus.campus.domain.inquiry.domain.entity.Inquiry;
 import com.campus.campus.domain.user.domain.entity.User;
 
 @Repository
 public interface InquiryRepository extends JpaRepository<Inquiry, Long> {
 	Page<Inquiry> findAllByWriterOrderByCreatedAtDesc(User writer, Pageable pageable);
+
+	Page<Inquiry> findAllByStudentCouncilWriterOrderByCreatedAtDesc(StudentCouncil council, Pageable pageable);
 }
