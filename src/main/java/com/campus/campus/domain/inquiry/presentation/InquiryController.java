4d70b7960c0f92365c103652a4cd91a5cec3aed4
wldy4627 <wldy4627@gmail.com>
2026-02-13 02:14:24 +0900

    refactor/#103: 일반 학생용 문의 API로 수정

--- src/main/java/com/campus/campus/domain/inquiry/presentation/InquiryController.java
+++ src/main/java/com/campus/campus/domain/inquiry/presentation/InquiryController.java
@@ -3,6 +3,7 @@ package com.campus.campus.domain.inquiry.presentation;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -17,11 +18,14 @@ import com.campus.campus.global.annotation.CurrentUserId;
 import com.campus.campus.global.common.response.CommonResponse;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/inquiries")
+@RequestMapping("users/inquiries")
+@PreAuthorize("hasRole('USER')")
+@Tag(name = "User Inquiry", description = "사용자(USER) 권한 전용 문의 API")
 @RequiredArgsConstructor
 public class InquiryController {
 
@@ -29,7 +33,7 @@ public class InquiryController {
 
 	@PostMapping
 	@Operation(
-		summary = "1:1 문의 등록",
+		summary = "일반 학생용 1:1 문의 등록",
 		description = "제목과 내용을 입력하여 새로운 문의를 등록합니다."
 	)
 	public CommonResponse<InquiryCreateResponse> create(
@@ -42,7 +46,7 @@ public class InquiryController {
 
 	@GetMapping("/me")
 	@Operation(
-		summary = "내 문의 내역 조회",
+		summary = "일반 학생의 문의 내역 조회",
 		description = "내가 작성한 문의 목록을 최신순으로 페이징하여 조회합니다. 아코디언 UI를 위해 상세 내용과 답변을 포함합니다."
 	)
 	public CommonResponse<Page<InquiryListItemResponse>> getMyInquiries(
