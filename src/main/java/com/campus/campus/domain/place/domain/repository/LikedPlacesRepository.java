0cc775b360e5bfa9ae4f571cd67140fc1ff723e6
wooodypark <harry122226@gmail.com>
2026-03-20 22:19:16 +0900

    hotfix: fix findLikedPlaceKeys query

--- src/main/java/com/campus/campus/domain/place/domain/repository/LikedPlacesRepository.java
+++ src/main/java/com/campus/campus/domain/place/domain/repository/LikedPlacesRepository.java
@@ -32,11 +32,13 @@ public interface LikedPlacesRepository extends JpaRepository<LikedPlace, Long> {
 	boolean existsByUserAndPlace(User user, Place place);
 
 	@Query("""
-		    SELECT COALESCE(lp.place.placeKey, lp.nonPartnerPlace.placeKey)
+		    SELECT COALESCE(p.placeKey, np.placeKey)
 		    FROM LikedPlace lp
+		    LEFT JOIN lp.place p
+		    LEFT JOIN lp.nonPartnerPlace np
 		    WHERE lp.user.id = :userId
-		      AND (lp.place.placeKey IN :placeKeys
-		           OR lp.nonPartnerPlace.placeKey IN :placeKeys)
+		      AND (p.placeKey IN :placeKeys
+		           OR np.placeKey IN :placeKeys)
 		""")
 	Set<String> findLikedPlaceKeys(@Param("userId") Long userId, @Param("placeKeys") List<String> placeKeys);
 
